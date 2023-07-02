965c6670cc482328be6cc02a7aaacfa0fac8e5c2
woohoocheezy <woohoocheezy@gmail.com>
2023-07-02 12:12:57 +0900

    Local changes

---
+++ config/__init__.py
@@ -0,0 +1,3 @@
+from .celery_push_noti import app as celery_app
+
+__all__ = ("celery_app",)
---
+++ fcm.py
@@ -0,0 +1,3 @@
+from push_notification.tasks import send_push_notifications
+
+send_push_notifications()
