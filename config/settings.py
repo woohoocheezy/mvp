06641a4c8eb2648ac8852d9cb430e50e17e3de5a
woohoocheezy <woohoocheezy@gmail.com>
2023-09-13 08:20:36 +0000

    Apply the thumbnail feature to backend test server

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["52.79.159.145", "localhost", "127.0.0.1", "0.0.0.0"]
 
