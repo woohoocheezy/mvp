73cdae1aab9288b61ce18b0dd41109332baca83d
woohoocheezy <woohoocheezy@gmail.com>
2023-11-17 11:53:20 +0900

    ADD the unique file name while uploading the image file on S3

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,5 @@
 import requests, mimetypes, os, imghdr, boto3
+from uuid import uuid4
 from botocore.exceptions import NoCredentialsError
 from django.conf import settings
 from rest_framework.views import APIView
@@ -49,41 +50,6 @@ class GetCFUploadURL(APIView):
 
 class GetUploadURL(APIView):
     def post(self, request):
-        """
-        # CloudFlare images upload
-        if "file" not in request.FILES:
-            return Response({"error": "No file provided"}, status=400)
-
-        uploaded_file = request.FILES["file"]
-        # print(uploaded_file)
-
-        # Call cloudflare API for getting the upload URL
-        url = f"https://api.cloudflare.com/client/v4/accounts/{settings.CF_ID}/images/v2/direct_upload"
-        url_request = requests.post(
-            url, headers={"Authorization": f"Bearer {settings.CF_TOKEN}"}
-        )
-
-        # Extract the upload URL from response body
-        response_data = url_request.json()
-        # print(response_data)
-        upload_url = response_data["result"]["uploadURL"]
-
-        # print(upload_url)
-
-        # Upload the file to the URL using requests
-        with uploaded_file.open("rb") as f:
-            file = {"file": (uploaded_file.name, f)}
-            response = requests.post(upload_url, files=file)
-
-        uploaded_url = response.json()["result"]["variants"][0]
-        # print(response.text)
-
-        return Response(
-            {"upload_url": uploaded_url},
-            status=200,
-        )
-        """
-
         # S3 upload
         if "file" not in request.FILES:
             return Response({"error": "No file provided"}, status=400)
@@ -97,13 +63,15 @@ class GetUploadURL(APIView):
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
         )
 
+        # Create unique file name
+        file_name = str(uuid4()) + "_" + uploaded_file.name
+
+        # Example of the file path depending on 'profile' or 'product' image
+        # the case of profile
+        # file_path = "profile/" + file_name
+
         # Upload image file to S3
         try:
-            # s3.upload_file(
-            #     uploaded_file,
-            #     settings.AWS_STORAGE_BUCKET_NAME,
-            #     uploaded_file.name,
-            # )
             s3.upload_fileobj(
                 uploaded_file,
                 settings.AWS_STORAGE_BUCKET_NAME,
@@ -114,6 +82,8 @@ class GetUploadURL(APIView):
         except NoCredentialsError:
             return Response({"error": "Credentials not available"}, status=400)
 
-        uploaded_url = f"https://{settings.AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{uploaded_file.name}"
+        uploaded_url = (
+            f"https://{settings.AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{file_name}"
+        )
 
         return Response({"uploaded_url": uploaded_url}, status=200)
